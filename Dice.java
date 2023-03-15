8948dbe07af33643aef4cb1a311ebcda041c7a51
ankitSarwar <111841677+ankitSarwar@users.noreply.github.com>
2023-03-15 19:37:32 +0530

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,10 @@
+package SnakeAndLadder;
+import java.util.*;
+public class Dice {
+    public static int roll() {
+        int MIN = 1, MAX = 6;
+        double r = Math.random();
+        int num = (int)(r * MAX) + MIN;
+        return num;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,18 @@
+package SnakeAndLadder;
+
+public class Player {
+    String name;
+    String colour;
+
+    Player(String name, String colour) {
+        this.name = name;
+        this.colour = colour;
+    }
+
+    @Override 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: ").append(this.name).append(", Colour: ").append(this.colour).append("\n");
+        return sb.toString();
+    }
+}
